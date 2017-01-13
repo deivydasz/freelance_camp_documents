25ecdc7c8c9f0c584a8a18aed6a43cec603de6e3
ecuSinner <deivydas.ziziliauskas@gmail.com>
2017-01-13 12:20:02 +0200

    Added data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# create_table "freelance_documents", force: :cascade do |t|
+#   t.string   "title"
+#   t.string   "description"
+#   t.text     "file_url"
+#   t.text     "image_url"
+#   t.datetime "created_at",  null: false
+#   t.datetime "updated_at",  null: false
+# end
+
+10.times do |d|
+	FreelanceDocument.create!(
+		title: "Document #{d}", 
+		description: "Brunch YOLO tote bag, trust fund migas sartorial austin keffiyeh put a bird on it. XOXO small batch banjo tumeric normcore, food truck hashtag lumbersexual fixie chillwave hoodie succulents.",
+		file_url: "https://hipsum.co/?paras=4&type=hipster-centric",
+		image_url: "https://udemy-images.udemy.com/course/750x422/54155_ffa4_6.jpg",
+		)
+end
+
+puts "10 Documents created!"
